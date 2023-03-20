fdf1bf31e61a884d83da2ebe9e47efb41cc75a59
nathancomley <100770485+nathancomley@users.noreply.github.com>
2023-03-20 17:26:40 +0800

    added code

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var addTaskButton = document.getElementById("add-task");
+var newTaskInput = document.getElementById("add-input");
+var tpdoListContainer = document.getElementById("todo-list");
 
+
+function onAddTaskClicked(event) {
+    var taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    todoListContainer.insertAdjustmentHTML('afterbegin',taskName);
+}
+
+
+addTaskButton.addEventListener('click' onAddTaskClicked)
